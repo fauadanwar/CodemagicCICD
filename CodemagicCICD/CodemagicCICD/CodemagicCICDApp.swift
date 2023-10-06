8ef5d840f52b57ea954079ab3b2244b717c518b3
fauad.anwar <fauad.anwar@gmail.com>
2023-10-06 16:38:55 +0530

    Adding CodemagicCICD project with Codemagic yml file and sample test case.

---
+++ CodemagicCICD/CodemagicCICD/CodemagicCICDApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodemagicCICDApp.swift
+//  CodemagicCICD
+//
+//  Created by fanwar on 30/09/23.
+//
+
+import SwiftUI
+
+@main
+struct CodemagicCICDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
